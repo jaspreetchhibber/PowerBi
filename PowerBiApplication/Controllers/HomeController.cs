fcbb02e351186c6ebd5d6f707c0d64ddeefb9dd1
jaspreet singh <39166752+jaspreetchhibber@users.noreply.github.com>
2020-10-07 19:48:49 +0530

    First Commit

---
+++ PowerBiApplication/Controllers/HomeController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.PowerBI.Api;
+using Microsoft.PowerBI.Api.Models;
+using Microsoft.Rest;
+using Newtonsoft.Json;
+using PowerBiApplication.Models;
+using PowerBiApplication.PowerBI;
+
+namespace PowerBiApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public string AccessToken { get; set; }
+        public AppSettings AppSettings { get; }
+        IConfiguration _iconfiguration;
+
+        public static string BaseUrl
+        {
+            get
+            {
+                return "https://login.microsoftonline.com/common/oauth2/token";
+            }
+        }
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration iconfiguration)
+        {
+            _logger = logger;
+            _iconfiguration = iconfiguration;
+            AppSettings = _iconfiguration.GetSection("AppSettings").Get<AppSettings>();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await GetPowerBIAccessToken();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public async Task<IActionResult> GetPowerBIAccessToken()
+        {
+            AccessToken=await GetAccessToken();
+
+            using (var client = new PowerBIClient(new Uri(AppSettings.ApiUrl), new TokenCredentials(AccessToken, "Bearer")))
+
+            {
+
+                var workspaceId = Guid.Parse("084c63d2-08b8-4584-8355-9e0db915ff7b");//24d84fc1-27b9-491b-bf11-9c5e79d3ec1d
+
+                var reportId = Guid.Parse("d9a989d6-cc20-4614-be56-554eb943b9c1");//9df5a78c-001f-479c-ad46-676da2a32988
+
+                var report = await client.Reports.GetReportInGroupAsync(workspaceId, reportId);
+
+                try
+
+                {
+
+                    var generateTokenRequestParameters = new GenerateTokenRequest(accessLevel: "view");
+
+                    var tokenResponse = await client.Reports.GenerateTokenAsync(workspaceId, reportId, generateTokenRequestParameters);
+
+                }
+
+                catch (Exception ex)
+
+                {
+
+                }
+
+                return Ok(new { token = AccessToken, embedUrl = report.EmbedUrl });
+
+            }
+
+        }
+
+        public static async Task<HttpResponseMessage> MakeAsyncRequest(string url, Dictionary<string, string> content)
+        {
+            var httpClient = new HttpClient
+            {
+                Timeout = new TimeSpan(0, 5, 0),
+                BaseAddress = new Uri(url)
+            };
+
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type: application/x-www-form-urlencoded", "application/json");
+
+            if (content == null)
+            {
+                content = new Dictionary<string, string>();
+            }
+
+            var encodedContent = new FormUrlEncodedContent(content);
+
+            var result = await httpClient.PostAsync(httpClient.BaseAddress, encodedContent);
+
+            return result;
+        }
+
+        private async Task<string> GetAccessToken()
+        {
+            var url = BaseUrl;
+            var content = new Dictionary<string, string>();
+            content["grant_type"] = "password";
+            content["resource"] = "https://analysis.windows.net/powerbi/api";
+            content["username"] = AppSettings.UserName;
+            content["password"] = AppSettings.Password;
+            content["client_id"] = AppSettings.ApplicationId;
+
+            var response = await MakeAsyncRequest(url, content);
+            var tokenresult = response.Content.ReadAsStringAsync().Result;
+            var AAD = JsonConvert.DeserializeObject<PowerBI.Models.AAD>(tokenresult);
+
+            return AAD.AccessToken;
+        }
+    }
+}
---
+++ PowerBiApplication/PowerBI/Models/AppSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PowerBiApplication.PowerBI
+{
+    public class AppSettings
+    {
+        public string AuthorityUri { get; set; }
+        public string ResourceUrl { get; set; }
+        public string RedirectUrl { get; set; }
+        public string ApiUrl { get; set; }
+        public string ApplicationId { get; set; }
+        public Guid GroupId { get; set; }
+        public string ReportId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
